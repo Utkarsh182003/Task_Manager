aada25db19c65cf40d9ab0bdff07a5fb9d2bceb7
Utkarsh182003 <utkarshpathak11up@gmail.com>
2024-05-01 15:15:30 +0530

    fix errors

--- src/router.tsx
+++ src/router.tsx
@@ -7,20 +7,16 @@ import AddTask from "./pages/AddTask";
 import ImportExport from "./pages/ImportExport";
 import Home from "./pages/Home";
 import TaskDetails from "./pages/TaskDetails";
-import SharePage from "./pages/Share";
-// import Purge from "./pages/Purge";
 
 const AppRouter = (): ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/task/:id" element={<TaskDetails />} />
-      <Route path="/share" element={<SharePage />} />
       <Route path="/add" element={<AddTask />} />
       <Route path="/user" element={<UserSettings />} />
       <Route path="/import-export" element={<ImportExport />} />
       <Route path="/categories" element={<Categories />} />
-      {/* <Route path="/purge" element={<Purge />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
